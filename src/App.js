540059fce18f31b1338462cf4199e26a06df465e
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-06-10 11:47:35 -0600

    added BrowserRouter function in index.js file to allow routing and added some base styling for navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 import data from "./data";
-import {Router} from 'react-router-dom'
+import {Route} from 'react-router-dom'
 import Main from "./pages/Main";
 import Stocks from "./pages/Stocks";
 import About from "./pages/About";
@@ -9,9 +9,11 @@ import Nav from "./components/Nav";
 
 export default function App() {
 	return (
-		<div className="App">
-			<h1>Hello CodeSandbox</h1>
-			<h2>Start editing to see some magic happen!</h2>
-		</div>
+		<>
+			<Nav />
+      <Route exact path='/'>
+        <Main />
+      </Route>
+		</>
 	);
 }
