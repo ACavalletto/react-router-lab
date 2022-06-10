efefd9928f3e39dba4b1accdd33214726d697b66
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-06-10 11:28:21 -0600

    Created basic file structure and basic JSX structure for pages as well as JSX for Nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./styles.css";
+import data from "./data";
+import {Router} from 'react-router-dom'
+import Main from "./pages/Main";
+import Stocks from "./pages/Stocks";
+import About from "./pages/About";
+import CompanyStock from "./pages/CompanyStock";
+import Nav from "./components/Nav";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Hello CodeSandbox</h1>
+			<h2>Start editing to see some magic happen!</h2>
+		</div>
+	);
 }
---
+++ src/pages/CompanyStock.js
@@ -0,0 +1,7 @@
+export default function CompanyStock() {
+	return (
+		<>
+			<h1>Company Stock</h1>
+		</>
+	);
+}
